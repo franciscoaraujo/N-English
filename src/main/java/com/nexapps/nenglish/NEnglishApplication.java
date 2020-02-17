9dbe37f9b065a8bc6d278aba86e4fa573fd6010b
Oliveira Araujo <foaj@gft.com>
2020-02-17 11:57:07 -0300

    commit inicial, criando repositorio do projeto

---
+++ src/main/java/com/nexapps/nenglish/NEnglishApplication.java
@@ -0,0 +1,13 @@
+package com.nexapps.nenglish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NEnglishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NEnglishApplication.class, args);
+	}
+
+}
