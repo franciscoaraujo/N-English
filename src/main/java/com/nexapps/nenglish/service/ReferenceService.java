b3f8afa78c47516108379c8d959e443c3bf81a58
foaj <foaj@BRPC003901.mshome.net>
2020-02-18 09:11:38 -0300

    Adicionando o service ReferenceService

--- src/main/java/com/nexapps/nenglish/service/ReferenceService.java
+++ src/main/java/com/nexapps/nenglish/service/ReferenceService.java
@@ -4,35 +4,31 @@ import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.databind.JsonMappingException.Reference;
 import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
-import com.nexapps.nenglish.domain.Association;
 import com.squareup.okhttp.Response;
 
-@Service
-public class AssociationService {
+public class ReferenceService {
 
-	@Value("${url.association}")
+	@Value("${url.reference}")
 	private String url;
-	
+
 	@Value("${access.twinword.host}")
 	private String host;
-	
+
 	@Value("${access.twinword.key}")
 	private String key;
-	
-	
+
 	@Autowired
 	private Connection conn;
-	
-	public Association request(String uri) throws JsonSyntaxException, IOException {
+
+	public Reference request(String uri) throws JsonSyntaxException, IOException {
 		Response response = conn.connection(url + uri, host, key);
 		Gson gson = new Gson();
 		String obj = response.body().string();
-		Association association = (Association) gson.fromJson(obj, Association.class);
-		return association;
+		Reference reference = gson.fromJson(obj, Reference.class);
+		return reference;
 	}
-
 }
